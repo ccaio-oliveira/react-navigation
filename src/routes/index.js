cee104017440a2a592e5e9f16d070dc4fdf17755
Caio Lucas <ccaio.oliveira83@gmail.com>
2023-02-20 11:27:14 -0300

    feat: adicionar bottom tabs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import Stack from './Routes';
+import Tab from './Tab';
 
 export default () => {
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <NavigationContainer>
-                <Stack />
+                <Tab />
             </NavigationContainer>
         </SafeAreaView>
     )
--- src/views/TelaC.js
+++ src/views/TelaC.js
@@ -1,7 +1,7 @@
 import TextoCentral from "../components/TextoCentral"
 
 export default ({route}) => {
-    const {numero} = route.params;
+    const numero = route && route.params && route.params.numero ? route.params.numero : 0;
     return(
         <TextoCentral corFundo='#9932cd'>
             Tela C - {numero}
